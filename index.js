14aa8c14a41ef96252949a035fb4d2b0ff342752
mmakash <musaddekurakash@gmail.com>
2023-10-01 20:08:33 +0600

    listening port added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 require("dotenv").config();
-const port = process.env.RUNNING_PORT || 5500;
+const port = process.env.RUNNING_PORT || 8000;
 app.listen(port,() =>{
     console.log(`Server is listening on ${port}`);
 })
