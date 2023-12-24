c5b4b45c2c0fb7ca0f04f6f602988d4b49361a68
akash <musaddekurakash@gmail.com>
2023-12-25 04:13:52 +0600

    cartlist added

--- src/Routes/api.js
+++ src/Routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ProductController = require('../controller/ProductController');
 const UserController = require('../controller/UserController');
 const WishListController = require('../controller/WishListController');
+const CartListController = require('../controller/CartListController');
 const AuthVerification = require('../middlewere/AuthVerification');
 
 
@@ -33,6 +34,12 @@ router.get("/ReadProfile",AuthVerification, UserController.ReadProfile);
 router.post("/SaveWishList",AuthVerification, WishListController.SaveWishList);
 router.post("/RemoveWishList",AuthVerification, WishListController.RemoveWishList);
 router.get("/WishList",AuthVerification, WishListController.WishList);
+
+// Cart
+router.post('/SaveCartList',AuthVerification,CartListController.SaveCartList)
+router.post('/RemoveCartList',AuthVerification,CartListController.RemoveCartList)
+router.get('/CartList',AuthVerification,CartListController.CartList)
+
 
 
 
