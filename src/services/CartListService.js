4046e329271445e156c3ad2b966bf259f29a517c
akash <musaddekurakash@gmail.com>
2023-12-26 04:08:46 +0600

    cart service completed

--- src/services/CartListService.js
+++ src/services/CartListService.js
@@ -4,33 +4,94 @@ const ObjectID=mongoose.Types.ObjectId
 
 const CartListService = async (req) => {
     try {
-
-
-    }catch (e) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
+        let user_id = new ObjectID(req.headers.user_id);
+        let matchStage = { $match: { userID: user_id } };
+    
+        let JoinStageProduct = {
+          $lookup: {
+            from: "products",
+            localField: "productID",
+            foreignField: "_id",
+            as: "product",
+          },
+        };
+        let unwindProductStage = { $unwind: "$product" };
+    
+        let JoinStageBrand = {
+          $lookup: {
+            from: "brands",
+            localField: "product.brandID",
+            foreignField: "_id",
+            as: "brand",
+          },
+        };
+        let unwindBrandStage = { $unwind: "$brand" };
+    
+        let JoinStageCategory = {
+          $lookup: {
+            from: "categories",
+            localField: "product.categoryID",
+            foreignField: "_id",
+            as: "category",
+          },
+        };
+        let unwindCategoryStage = { $unwind: "$category" };
+    
+        let projectionStage = {
+          $project: {
+            _id: 0,
+            userID: 0,
+            createAt: 0,
+            updatedAt: 0,
+            "product._id": 0,
+            "product.categoryID": 0,
+            "product.brandID": 0,
+            "brand._id": 0,
+            "category._id": 0,
+          },
+        };
+    
+        let data = await CartModel.aggregate([
+          matchStage,
+          JoinStageProduct,
+          unwindProductStage,
+          JoinStageBrand,
+          unwindBrandStage,
+          JoinStageCategory,
+          unwindCategoryStage,
+          projectionStage,
+        ]);
+    
+        return { status: "success", data: data };
+      } catch (e) {
+        return { status: "fail", message: "Something Went Wrong !" };
+      }
 }
 
 
 const SaveCartListService = async (req) => {
     try {
-
-
-
-    }
-    catch (e) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
+        let user_id = req.headers.user_id;
+        let reqBody = req.body;
+        reqBody.userID = user_id;
+        await CartModel.updateOne(reqBody, { $set: reqBody }, { upsert: true });
+        return { status: "success", message: "Wish List Save Success" };
+      } catch (e) {
+        return { status: "fail", message: "Something Went Wrong !" };
+      }
 }
 
 
 const RemoveCartListService = async (req) => {
     try {
-
-    }
-    catch (e) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
+        let user_id = req.headers.user_id;
+        let reqBody = req.body;
+        reqBody.userID = user_id;
+        await CartModel.deleteOne(reqBody);
+        return { status: "success", message: "Wish List Remove Success" };
+      } catch (e) {
+        return { status: "fail", message: "Something Went Wrong !" };
+      }
 }
 
 
